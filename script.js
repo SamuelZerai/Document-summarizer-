2b06df646c4a8f9899eaf02ca3879fa9df4f8165
SamuelZerai <Szerai03@gmail.com>
2026-03-03 19:20:18 +0100

    JS kod med API för llama

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const button = document.getElementById("summarize-btn");
+const textarea = document.getElementById("text-input");
 
-button.addEventListener("click", function() {
-    console.log("Knappen Klickades");
+
+
+button.addEventListener("click", async function () {
+    const text = textarea.value;
+   
+    if (text.trim() === "") {
+        alert("Klistra in din text innan du sammanfattar!");
+        return
+    }
+
+    const response = await fetch("http://localhost:11434/api/generate", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "llama3.2",
+            prompt: "Sammanfatta följande texter på svenska " + text,
+            stream: false
+        })
+    });
+
+    const data = await response.json();
+    console.log(data);
+
 });
